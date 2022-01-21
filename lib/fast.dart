f277b0b455bca87ea72f034804732570d16105eb
Kensai <moonshine@spacecorp.gg>
2022-01-21 21:00:37 +0100

    Oups missing dep

--- lib/fast.dart
+++ lib/fast.dart
@@ -9,6 +9,7 @@ export 'widgets/responsive.dart';
 export 'widgets/adaptive_scaffold.dart';
 export 'widgets/button_dispatcher.dart';
 export 'widgets/mouse_drag_scroll_behavior.dart';
+export 'widgets/ignore_keyboard.dart';
 
 export 'statics/nav.dart';
 export 'statics/screen.dart';

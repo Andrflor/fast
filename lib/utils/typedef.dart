d5b1e43dcd94d1a644d1f8bb349e6d7316ba101e
andrflor <andrieu.florian@mail.com>
2022-01-25 14:30:19 +0100

    Initial offset

--- lib/utils/typedef.dart
+++ lib/utils/typedef.dart
@@ -44,10 +44,13 @@ Worker runOnResize(WorkerCallback<Size> callback) {
 }
 
 mixin ScrollCapability on GetxController {
-  final ScrollController scroll = ScrollController(keepScrollOffset: false);
+  late final ScrollController scroll;
+
+  double get initialOffset;
 
   @override
   void onInit() {
+    ScrollController(initialScrollOffset: initialOffset);
     super.onInit();
     scroll.addListener(_listener);
     WidgetsBinding.instance?.addPostFrameCallback(
